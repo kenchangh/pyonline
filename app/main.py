e6e32cd910547350d6a652f465e2a082390daf16
Maverick Chan <guanhao3797@gmail.com>
2014-07-27 12:38:49 +0800

    Sandboxing and comments
    The function 'sandbox' separates the user's variables from clashing with
    the existing ones. Which means better security and less odd behaviours.
    
    I also wrote some comments for a better explanation what's going on.

--- app/main.py
+++ app/main.py
@@ -23,25 +23,47 @@ config['webapp2_extras.sessions'] = {
 ##########
 
 class Processor(BaseHandler):
+    """
+    Processes code in a sanbox environment.
+    Sends the response back in plain text.
+
+    Each user will need to access the API with a user_id, which is
+    generated on the client's side. 
+    EXAMPLE: http:/www.pyonline.appspot/<user_id>
+
+    The code will be sent via a POST request in a url encoded manner.
+    Then, accessed at the same url with a GET request.
+    EXAMPLE:
+    >> import urllib, urllib2
+    >> Urlencode key-value pair, where key must be 'code'
+    >> data = { 'code': "print 'hello world'" }
+    >> data = urlencode(data)
+    >> # Send POST request with extra data parameter in urlopen
+    >> urlopen('http://www.pyonline.appspot/userid', data)
+    >> result = urlopen('http://www.pyonline.appspot/userid')
+    >> result.read()
+    hello world
+    """
+    # To avoid changing local variables
+    def sandbox(self, code):
+        buffer = StringIO()
+        sys.stdout = buffer
+        exec code
+        sys.stdout = sys.__stdout__
+        result = buffer.getvalue()
+        return result
     
     def get(self, **kw):
         user_id = kw['user_id']
         result = memcache.get(user_id)
         logging.info(result)
-        self.response.headers['Content-Type'] = 'application/json'
-        result = {
-            'success': result
-        }
-        self.response.out.write(json.dumps(result))
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(result)
 
     def post(self, **kw):
         user_id = kw['user_id']
-        pycode = self.request.get('pycode')
-        buffer = StringIO()
-        sys.sydout = buffer
-        exec pycode
-        sys.stdout = sys.__stdout__
-        result = buffer.getvalue()
+        code = self.request.get('code')
+        result = self.sandbox(code)
         logging.info(result)
         memcache.set(user_id, result)
             
